417aa649059a3c117c2eda89e3d2590bfe4056f2
Tobi Lehman <tobi.lehman@gmail.com>
2012-10-29 13:08:39 -0700

    Convert all the tab chars to spaces

--- lib/pretty_param.rb
+++ lib/pretty_param.rb
@@ -1,20 +1,20 @@
 module PrettyParam
-	class << self
-		# This method does the actual permalink escaping.
-		def escape(string)
-			result = string
-			result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
-			result.gsub!(/[^\w_ \-]+/i, '') # Remove unwanted chars.
-			result.gsub!(/[ \-]+/i, '-') # No more than one of the separator in a row.
-			result.gsub!(/^\-|\-$/i, '') # Remove leading/trailing separator.
-			result.downcase
-		end
-	end
-		
-	module ClassMethods
-		# Takes 1 or more symbols and stores them in class.pretty_params,
-		# and then overrides to_param
-		def has_pretty_param(*fields)			
+  class << self
+    # This method does the actual permalink escaping.
+    def escape(string)
+      result = string
+      result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
+      result.gsub!(/[^\w_ \-]+/i, '') # Remove unwanted chars.
+      result.gsub!(/[ \-]+/i, '-') # No more than one of the separator in a row.
+      result.gsub!(/^\-|\-$/i, '') # Remove leading/trailing separator.
+      result.downcase
+    end
+  end
+    
+  module ClassMethods
+    # Takes 1 or more symbols and stores them in class.pretty_params,
+    # and then overrides to_param
+    def has_pretty_param(*fields) 
       if RUBY_VERSION < "1.9.3"
         singleton = class << self; self; end
         singleton.send :define_method, :pretty_params, lambda { fields }
@@ -22,11 +22,11 @@ module PrettyParam
         define_singleton_method :pretty_params, -> { fields }
       end
 
-			define_method :to_param do
-				PrettyParam.escape("#{self.id}-#{self.class.pretty_params.map{|a| self.send(a.to_sym) }.join('-')}")
-			end
-		end
-	end
+      define_method :to_param do
+        PrettyParam.escape("#{self.id}-#{self.class.pretty_params.map{|a| self.send(a.to_sym) }.join('-')}")
+      end
+    end
+  end
 end
 
 ActiveRecord::Base.extend PrettyParam::ClassMethods
--- spec/pretty_param_spec.rb
+++ spec/pretty_param_spec.rb
@@ -1,38 +1,38 @@
 require 'rails/all'
 require 'pretty_param'
 
-ActiveRecord::Base.establish_connection(adapter: "sqlite3",	database: "spec/test.db")
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "spec/test.db")
 ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS 'dummies'")
 ActiveRecord::Base.connection.create_table(:dummies) do |t|
-	t.string :first_name
-	t.string :last_name
-	t.string :nickname
+  t.string :first_name
+  t.string :last_name
+  t.string :nickname
 end
 
 describe ActiveRecord::Base do
-	it "should receive #has_pretty_param" do
-		ActiveRecord::Base.should respond_to(:has_pretty_param)
-	end
+  it "should receive #has_pretty_param" do
+    ActiveRecord::Base.should respond_to(:has_pretty_param)
+  end
 end
 
 class Dummy < ActiveRecord::Base
-	has_pretty_param :first_name, :last_name
+  has_pretty_param :first_name, :last_name
 end
 
 describe PrettyParam do
-	let(:dummy) { Dummy.create(first_name: "Alex", last_name: "Kiheri", nickname: "AFK") }
-	let(:blinged_dummy) { Dummy.create(first_name: "^A%l/ex@#", last_name: "#K+iheri!@") }
-	let(:multiple_space_dummy) { Dummy.create(first_name: "Alex ", last_name: " Kiheri") }
-	
-	it "should add specified attributes to `to_param`" do
-		dummy.to_param.should include("alex-kiheri")
-	end
-	
-	it "should filter out symbols and forward slashes" do
-		blinged_dummy.to_param.should include("alex-kiheri")
-	end
-	
-	it "should remove multiple hyphens" do
-			multiple_space_dummy.to_param.should include("alex-kiheri")
-	end
+  let(:dummy) { Dummy.create(first_name: "Alex", last_name: "Kiheri", nickname: "AFK") }
+  let(:blinged_dummy) { Dummy.create(first_name: "^A%l/ex@#", last_name: "#K+iheri!@") }
+  let(:multiple_space_dummy) { Dummy.create(first_name: "Alex ", last_name: " Kiheri") }
+  
+  it "should add specified attributes to `to_param`" do
+    dummy.to_param.should include("alex-kiheri")
+  end
+  
+  it "should filter out symbols and forward slashes" do
+    blinged_dummy.to_param.should include("alex-kiheri")
+  end
+  
+  it "should remove multiple hyphens" do
+      multiple_space_dummy.to_param.should include("alex-kiheri")
+  end
 end
